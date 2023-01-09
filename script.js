cc660a54e3f48fff9d27c9228c1c45daaf042d6f
Bobbybgit <rgboyce@gmail.com>
2023-01-09 15:17:43 +0000

    Javascript basics to set up calendar dates

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let pageDate = currentDate;
 const yearTitle = document.querySelector('#year');
 const monthTitle = document.querySelector('#month');
 
+const dayDivs = document.querySelectorAll('.day');  
+
 function monthToString(month){
     switch (month){
         case 0: return "January"
@@ -26,6 +28,30 @@ function setHeaders(showYear, showMonth){
     monthTitle.innerText = monthToString(showMonth);
 }
 
+function getDaysInMonth(date){
+    date.setMonth(date.getMonth()+1);
+    date.setDate(0);
+    return date.getDate();
+}
+
+function setDayDivs(settingDate){
+    let daysInMonth = getDaysInMonth(settingDate);
+    settingDate.setDate(1);
+    let incFirstDay = daysInMonth + settingDate.getDay();
+    let proxFirstDay = 0-settingDate.getDay();
+    for (let i=0;i < dayDivs.length;i++){
+        if (proxFirstDay < 0 || proxFirstDay >= daysInMonth){
+            dayDivs[i].innerText = "";
+            dayDivs[i].style['background-color'] = "#FFFFFF";
+        }else{
+            dayDivs[i].innerText = i+1;
+            dayDivs[i].style['background-color'] = "#FFD685";
+        }
+        proxFirstDay++
+    }
+}
+
 setHeaders(pageDate.getFullYear(), pageDate.getMonth());
+setDayDivs(pageDate);
 
 
